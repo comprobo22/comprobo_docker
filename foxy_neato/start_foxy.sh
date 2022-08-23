a704b5c4f0c4573fee58045a0c55358f8f1ff96e
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-08-23 10:53:49 -0400

    docker is looking good

--- foxy_neato/start_foxy.sh
+++ foxy_neato/start_foxy.sh
@@ -2,4 +2,5 @@
 
 docker build -t foxy_neato .
 xhost +local:
-docker run -it --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix foxy_neato
+# TODO: warning these ports would have to be updated if we use different ports to receive video and robot data (default UDP ports are 5000 and 7777, respectively)
+docker run -it -p 5000:5000/udp -p 7777:7777/udp -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix foxy_neato
