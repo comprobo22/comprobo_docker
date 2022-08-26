1ff16c66865e121cc21c9fc5050f373b6d08bf8d
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-08-25 23:58:16 -0400

    added Galactic support

---
+++ galactic_neato/start_galactic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t galactic_neato .
+xhost +local:
+# TODO: warning these ports would have to be updated if we use different ports to receive video and robot data (default UDP ports are 5000 and 7777, respectively)
+docker run -it -p 5000:5000/udp -p 7777:7777/udp -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix galactic_neato
---
+++ galactic_neato/start_galactic_nvidia.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Make sure you have installed nvidia container toolkit"
+
+docker build -t galactic_neato .
+xhost +local:
+# TODO: warning these ports would have to be updated if we use different ports to receive video and robot data (default UDP ports are 5000 and 7777, respectively)
+docker run --gpus=all --privileged -it -p 5000:5000/udp -p 7777:7777/udp -e DISPLAY=$DISPLAY -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID -v /tmp/.X11-unix:/tmp/.X11-unix galactic_neato
