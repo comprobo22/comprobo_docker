de515c7588fff9b617c8cdeb36afcd670510cea6
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-08-26 22:59:34 -0400

    humble support and remove tb3 build from source

---
+++ humble_neato/start_humble.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t humble_neato .
+xhost +local:
+# TODO: warning these ports would have to be updated if we use different ports to receive video and robot data (default UDP ports are 5000 and 7777, respectively)
+docker run -it -p 5000:5000/udp -p 7777:7777/udp -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix humble_neato
